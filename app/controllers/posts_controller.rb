a1a67285f5995acf56dcba2ac085fb79dfe79a01
su <suuuuu0202@gmail.com>
2018-04-01 12:31:12 +0900

    検索機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,50 @@
 class PostsController < ApplicationController
-  before_action :sign_in_required, only: [:show]
 
   def index
-    @posts = Post.all
+
+      @posts = Post.all
+      @posts = Post.search(params[:search])
   end
 
   def show
+    @post = Post.find_by(id: params[:id])
   end
 
   def new
   end
 
   def create
-    @post = Post.new(content: params[:content])
+    @post = Post.new(posts_params)
+    image = params[:posts][:image]
+    @post.image_name = image.original_filename
+    File.binwrite("public/post_images/#{image.original_filename}",image.read)
     @post.save
-    redirect_to("/posts/index")
+    redirect_to("/posts")
   end
 
   def update
+    debugger
+    @post = Post.find_by(id: params[:id])
+    @post.title = params[:title]
+    image = params[:posts][:image]
+    @post.image_name = image.original_filename
+    File.binwrite("public/post_images/#{image.original_filename}",image.read)
+    @post.content = params[:content]
+    @post.save
+    redirect_to("/posts")
   end
 
   def edit
+    @post = Post.find_by(id: params[:id])
   end
 
   def destroy
+  end
+
+  private
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def posts_params
+    params.require(:posts).permit(:title, :content)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,11 @@
 Rails.application.routes.draw do
+
   devise_for :users
 
+  resources :posts
   resources :users, :only => [:index, :show]
-
-  get "posts/new" => "posts#new"
-  get "posts/index" => "posts#index"
-  get "posts/:id" => "posts#show"
-  post "posts/create" => "posts#create"
-  get "posts/:id/edit" => "posts#edit"
-
-
-
 
   get "/" => "home#top"
   get "/about" => "home#about"
 
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
